a5a73fc1239a3a00fc131a9e6061fee8431e06fc
Brad Woodward <brad@bradwoodward.io>
2020-03-29 10:11:22 -0600

    NPM Init

--- userdata.sh
+++ userdata.sh
@@ -5,8 +5,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+yum install -y htop git
+
 nvm install 12
 mkdir /tmp/warcannon
 chmod 777 /tmp/warcannon
 
-sudo mount -t tmpfs -o size=300g ramdisk /tmp/warcannon
+sudo mount -t tmpfs -o size=200g ramdisk /tmp/warcannon
+git clone https://github.com/c6fc/warcannon.git
